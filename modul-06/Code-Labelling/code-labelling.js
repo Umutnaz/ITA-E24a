106a90f059807b3d5fdec2ac406acceedaf739e6
Thomas <thsp@eaaa.dk>
2024-11-07 17:17:55 +0100

    Updates

--- modul-06/Code-Labelling/code-labelling.js
+++ modul-06/Code-Labelling/code-labelling.js
@@ -1,7 +1,29 @@
-for (let i = 2; i < 21; i += 2) {}
+const car = {
+  speed: 0,
+  color: "Red",
+  accelerate: function () {
+    this.speed += 5;
+    console.log(this.speed);
+    return this;
+  },
+  break: function () {
+    this.speed -= 5;
+    console.log(this.speed);
+    return this;
+  },
+  honk: function () {
+    console.log("HOOOOONK!!!!!!");
+    return this;
+  },
+  toString: () => {
+    return this.color + " " + this.speed;
+  },
+};
 
-const mainContentElement = document.getElementById("main-content-area");
+car.honk().honk().accelerate().accelerate().honk().honk().break().break();
 
-let authorToInsert = "<p class='author'>" + data[i].author + "</p>";
+let authorToInsert = "<p class='car'>" + car.toString() + "</p>";
 
 while (i < 10) {}
+
+
