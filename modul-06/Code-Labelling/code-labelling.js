a3b8c84429948f9cfe0db2e000ab960ca1ee958b
Thomas <thsp@eaaa.dk>
2024-11-07 14:42:57 +0100

    Ready for teaching

---
+++ modul-06/Code-Labelling/code-labelling.js
@@ -0,0 +1,7 @@
+for (let i = 2; i < 21; i += 2) {}
+
+const mainContentElement = document.getElementById("main-content-area");
+
+let authorToInsert = "<p class='author'>" + data[i].author + "</p>";
+
+while (i < 10) {}
